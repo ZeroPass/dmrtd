8f201c80234928fe3392c9f69245989a7f4981e1
smlu <smlu@s5.net>
2020-10-25 21:14:38 +0100

    Update bac.dart

--- lib/src/proto/bac.dart
+++ lib/src/proto/bac.dart
@@ -34,9 +34,9 @@ class BAC {
   // Specified in section 4.3.1 of ICAO 9303 p11 doc
   static const nonceLen =  8;                        // Challenge is 8 bytes
   static const kLen     = 16;                        // Key length 16 bytes
-  static const sLen     =  2 * nonceLen + kLen;      // S length
+  static const sLen     = (2 * nonceLen) + kLen;     // S length
   static const rLen     = sLen;                      // R length
-  static const eLen     = sLen;                      // Encrypted cryptogram S length 16 bytes
+  static const eLen     = sLen;                      // Encrypted cryptogram S length 32 bytes
   static const macLen   = ISO9797.macAlg3_DigestLen; // 8 bytes
 
   static Future<void> initSession({ @required DBAKeys dbaKeys, @required ICC icc }) async {
