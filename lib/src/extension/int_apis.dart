f1a26348f926b14548a9dc79c4741811604883d0
smlu <smlu@s5.net>
2021-05-12 14:40:49 +0200

    Format var name

--- lib/src/extension/int_apis.dart
+++ lib/src/extension/int_apis.dart
@@ -3,7 +3,7 @@
 extension IntApis on int {
   String hex() {
     final str = toRadixString(16);
-    final padded_len = (str.length.isOdd ? 1 : 0) + str.length;
-    return str.padLeft(padded_len, '0').toUpperCase();
+    final paddedLen = (str.length.isOdd ? 1 : 0) + str.length;
+    return str.padLeft(paddedLen, '0').toUpperCase();
   }
 }
